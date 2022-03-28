33b1f4799d522891f53e951844f05bf39e7582d1
Md.Hamid Hosen <68488154+hamidhosen42@users.noreply.github.com>
2022-03-28 14:52:55 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Component/Header/Header';
+import Pricing from './Component/Pricing/Pricing';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <h1 className="text-6xl">Welcome to my pricing club</h1>
+      <Pricing/>
+    </div>
+  );
+}
+
+export default App;
